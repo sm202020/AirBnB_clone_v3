b4ce5b0a9fe5dccc8aff7ed009b33831f16bd2f0
domgeorge <obinnadom007@gmail.com>
2024-05-27 07:20:31 -0700

    Updated app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,5 +23,5 @@ def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(debug=True, host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')), threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
