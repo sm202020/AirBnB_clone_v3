84ff208b5a38d9186c138223318b182b373fb68f
domgeorge <obinnadom007@gmail.com>
2024-05-27 07:16:14 -0700

    Updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-from os import getenv
+import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
