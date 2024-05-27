3361df1c43b9aafed406cbdf5f2377f6d6c43c1d
domgeorge <obinnadom007@gmail.com>
2024-05-27 06:32:57 -0700

    Updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-import os
+from os import getenv
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
@@ -23,5 +23,5 @@ def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    app.run(debug=True, host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')), threaded=True)
